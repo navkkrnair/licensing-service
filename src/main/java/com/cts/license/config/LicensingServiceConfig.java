beca2e936f57ce001483f9afdd38d836be6842f6
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-18 10:27:35 +0530

    Added interceptor

--- src/main/java/com/cts/license/config/LicensingServiceConfig.java
+++ src/main/java/com/cts/license/config/LicensingServiceConfig.java
@@ -1,13 +1,17 @@
 package com.cts.license.config;
 
+import com.cts.license.utils.UserContextInterceptor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
@@ -31,6 +35,14 @@ public class LicensingServiceConfig {
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        if (interceptors == null) {
+            restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            restTemplate.setInterceptors(interceptors);
+        }
+        return restTemplate;
     }
 }
---
+++ src/main/java/com/cts/license/utils/UserContextInterceptor.java
@@ -0,0 +1,23 @@
+package com.cts.license.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+@Slf4j
+public class UserContextInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        HttpHeaders headers = request.getHeaders();
+        String correlationId = UserContextHolder.getContext()
+                                                .getCorrelationId();
+        headers.add(UserContext.CORRELATION_ID, correlationId);
+
+        return execution.execute(request, body);
+    }
+}
